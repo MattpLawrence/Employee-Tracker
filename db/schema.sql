e4460a2025e205ebd6071e9fea250ec1b0feacb9
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-27 16:30:59 -0500

    update shema and seeds to use string for insert data

--- db/schema.sql
+++ db/schema.sql
@@ -2,26 +2,29 @@ CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
-CREATE TABLE employee (
+CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NUL,
-  manager_id INT
-  FOREIGN KEY (role_id)
-  REFERENCES role(id),
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
 );
 
-CREATE TABLE department(
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
 );
+
+
+
