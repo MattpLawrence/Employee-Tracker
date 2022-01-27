091722bbb425e5cfdf97606a2d7179ce902605ba
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-27 15:32:41 -0500

    add inquirer

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const express = require("express");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+//require hidden information form .env file
+require("dotenv").config();
+const PORT = DB_HOST;
