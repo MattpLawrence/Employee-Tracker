a47f965eb9bc6effeb9383d402db850b08edc1c7
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-27 16:18:46 -0500

    add seeds

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,10 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  rile_id INT NOT NUL,
+  role_id INT NOT NUL,
   manager_id INT
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
 );
 
 CREATE TABLE role (
@@ -15,4 +17,11 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL
-)
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES (),()
+
+INSERT INTO role(title,salary,department_id)
+VALUES(Chief marketing officer, 200000, 1),
+      (Creative director, 150000, 1),
+      (Marketing Manager, 100000, 1),
+      (Content specialist, 80000, 1),
+      (VP of Operations, 200000, 2),
+      (Chief Operations officer , 150000, 2),
+      (Director of Operations, 100000, 2),
+      (Operations Engineer, 80000, 2),
+      (Investment Manager, 120000, 3),
+      (Financial analyst, 100000, 3),
+      (Financial advisor. 80000,3),
+      (Accountant, 60000, 3),
+      (Account Manager, 120000, 4),
+      (Client Serveces, 100000, 4),
+      (Sales support, 80000, 4),
+      (Outside Sales, 60000, 4),
+      (Inside Sales, 60000, 4)
+
+INSERT INTO department(name)
+VALUES(Marketing),
+      (Operations),
+      (Finance),
+      (Sales),
+      (Purchasing),
+      (Human Resources)
